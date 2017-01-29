9a3a34f30bf66afea8d19bb6049c955240022f00
Cameron Regan <cameronthomasregan@gmail.com>
2017-01-29 19:45:44 +1100

    Changed single-quotes to double quotes - reimplemented the += for gmt method.

--- modules.rb
+++ modules.rb
@@ -7,60 +7,60 @@ require 'pp'
 module Cric
   def self.gmt(time = nil) # don't uselocal_to_utc - doesn't make sense
     tz = TZInfo::Timezone.get('Australia/Sydney')
-    time = ' UTC'
+    time += ' UTC'
     time = Time.parse(time)
     time = tz.local_to_utc(time)
     tz.strftime('%Y-%m-%d %H:%M:%S %Z', time)
   end
 
   def self.fetch_matches
-    uri = URI('http://Cricapi.com/api/matches/?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2')
-    JSON.parse(Net::HTTP.get(uri))['matches']
+    uri = URI("http://cricapi.com/api/matches/?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2")
+    JSON.parse(Net::HTTP.get(uri))["matches"]
   end
 
   def self.fetch_score(id)
-    uri = URI('http://Cricapi.com/api/CricketScore?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2&unique_id=#{id}')
+    uri = URI("http://cricapi.com/api/cricketScore?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2&unique_id=#{id}")
     JSON.parse(Net::HTTP.get(uri))
   end
 
   def self.cricket_score(team) # Dont set team=nil, its not optional
-    id = fetch_matches.find { |h| h.key(team) }['unique_id']
+    id = fetch_matches.find { |h| h.key(team) }["unique_id"]
 
     score = fetch_score(id)
     result = {}
 
-    if score.key?('score') && ['score'].include?('Match')
-      result[:final] = score['innings-requirement'] << '    '
+    if score.key?("score") && ["score"].include?("Match")
+      result[:final] = score["innings-requirement"] << "    "
       result[:final]
-    elsif score['innings-requirement'].include?('scheduled') && score['team-2'].include?(team)
-      result[:schedule] = score['innings-requirement']
-      result[:team] = score['team-1']
-      result[:date_raw] = Time.parse(gmt(score['dateTimeGMT']))
-      result[:date] = result[:date_raw].strftime('%B %-d, %Y')
-      result[:final] = '#{result[:schedule]} against #{result[:team]} on #{result[:date]}.' << '  '
+    elsif score["innings-requirement"].include?("scheduled") && score["team-2"].include?(team)
+      result[:schedule] = score["innings-requirement"]
+      result[:team] = score["team-1"]
+      result[:date_raw] = Time.parse(gmt(score["dateTimeGMT"]))
+      result[:date] = result[:date_raw].strftime("%B %-d, %Y")
+      result[:final] = "#{result[:schedule]} against #{result[:team]} on #{result[:date]}." << "  "
       result[:final]
-    elsif score['innings-requirement'].include?('scheduled') && score['team-1'].include?(team)
-      result[:schedule] = score['innings-requirement']
-      result[:team] = score['team-2']
-      result[:date_raw] = Time.parse(gmt(score['dateTimeGMT']))
-      result[:date] = result[:date_raw].strftime('%B %-d, %Y')
-      result[:final] = '#{result[:schedule]} against #{result[:team]} on #{result[:date]}.' << ' '
+    elsif score["innings-requirement"].include?("scheduled") && score["team-1"].include?(team)
+      result[:schedule] = score["innings-requirement"]
+      result[:team] = score["team-2"]
+      result[:date_raw] = Time.parse(gmt(score["dateTimeGMT"]))
+      result[:date] = result[:date_raw].strftime("%B %-d, %Y")
+      result[:final] = "#{result[:schedule]} against #{result[:team]} on #{result[:date]}." << " "
       result[:final]
-    elsif score['matchStarted'] == true && !(score['innings-requirement'].include?('toss')) && !(score['innings-requirement'].include?('won'))
-      score_dirty = score['score']
+    elsif score["matchStarted"] == true && !(score["innings-requirement"].include?("toss")) && !(score["innings-requirement"].include?("won"))
+      score_dirty = score["score"]
       score_clean = score_dirty.sub(/^([\w ]+) (\d+)\/(\d+)/, '\1 \3/\2')
       rr = Cric.runrate(score_clean)
-      required = score['innings-requirement']
-      result[:final] = '#{score_clean} - #{rr} RPO. \n \n#{required}'
+      required = score["innings-requirement"]
+      result[:final] = "#{score_clean} - #{rr} RPO. \n \n#{required}"
       result[:final]
-    elsif score['matchStarted'] == true && !(score['innings-requirement'].include?('toss')) && score['innings-requirement'].include?('won')
-      result[:final] = score['innings-requirement']
+    elsif score["matchStarted"] == true && !(score["innings-requirement"].include?("toss")) && score["innings-requirement"].include?("won")
+      result[:final] = score["innings-requirement"]
       result[:final]
-    elsif score['matchStarted'] == true
-      score_dirty = score['score']
-      rr = Cric.runrate(score_dirty)
+    elsif score["matchStarted"] == true
+      score_dirty = score["score"]
+      rr = self.runrate(score_dirty)
       score_clean = score_dirty.sub(/^([\w ]+) (\d+)\/(\d+)/, '\1 \3/\2')
-      result[:final] = '#{score_clean} \n \n#{rr} RPO'
+      result[:final] = "#{score_clean} \n \n#{rr} RPO"
       result[:final]
     end
   end
