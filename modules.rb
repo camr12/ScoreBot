4b0e986389ace1d11b2c33a68f460c978849ee60
Cameron Regan <cameronthomasregan@gmail.com>
2017-01-30 20:02:23 +1100

    replaced gmt function with gmtdate and gmttime

--- modules.rb
+++ modules.rb
@@ -2,25 +2,47 @@ require 'json'
 require 'tzinfo'
 require 'active_support/core_ext/enumerable'
 require 'pp'
+require 'time'
+require 'uri'
+require 'net/http'
 
 # Module for checking cricket scores/schedules using the CricAPI
 module Cric
-  def self.gmt(time = nil) # don't uselocal_to_utc - doesn't make sense
-    tz = TZInfo::Timezone.get('Australia/Sydney')
-    time += ' UTC'
-    time = Time.parse(time)
-    time = tz.local_to_utc(time)
-    tz.strftime('%Y-%m-%d %H:%M:%S %Z', time)
-  end
+#  def self.gmt(time = nil) # don't use local_to_utc - doesn't make sense
+#    tz = TZInfo::Timezone.get('Australia/Sydney')
+#    time += ' UTC'
+#    time = Time.parse(time)
+#    time = tz.local_to_utc(time)
+#    tz.strftime('%Y-%m-%d %H:%M:%S %Z', time)
+#  end
+
+#def self.gmt(t = nil) # convert GMT time to Sydney time (AEDT/AEST)
+#  t2 = tz.utc_to_local(t1)
+#  #t2 = t1 + t1.utc_offset # parse gmt_offset to local Sydney time
+#  t3 = t2.strftime("%F %T") # remove redundant gmt_offset
+#  t4 = t3.strftime("%I:%M %p") # convert to "hour:second PM/AM"
+#end
+
+def self.gmttime(tstr) # convert GMT time to timezone defined by tz variable
+  tz = TZInfo::Timezone.get('Australia/Sydney') # timezone to convert to
+  time = Time.parse(tstr) # parse time 
+  tz.utc_to_local(time).strftime("%I:%M %p") # convert to "hour:second AM/PM" and timezone
+end
+
+def self.gmtdate(tstr)
+  tz = TZInfo::Timezone.get('Australia/Sydney') # timezone to convert to
+  time = Time.parse(tstr) # parse time 
+  tz.utc_to_local(time).strftime("%B %d, %Y") # convert to "hour:second AM/PM" and timezone
+end
 
   def self.fetch_matches
-    uri = URI('http://cricapi.com/api/matches/?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2')
-    JSON.parse(Net::HTTP.get(uri))['matches']
+    uri = URI('http://cricapi.com/api/matches/?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2') # fetch json for match list
+    JSON.parse(Net::HTTP.get(uri))['matches'] # parse match list as hash
   end
 
   def self.fetch_score(id)
-    uri = URI("http://cricapi.com/api/cricketScore?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2&unique_id=#{id}")
-    JSON.parse(Net::HTTP.get(uri))
+    uri = URI("http://cricapi.com/api/cricketScore?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2&unique_id=#{id}") # fetch json for matchs core
+    JSON.parse(Net::HTTP.get(uri)) # parse score list as hash
   end
 
   def self.cricket_score(team) # Dont set team=nil, its not optional
@@ -35,22 +57,20 @@ module Cric
     elsif score['innings-requirement'].include?('scheduled') && score['team-2'].include?(team)
       result[:schedule] = score['innings-requirement']
       result[:team] = score['team-1']
-      result[:time_raw] = Time.parse(gmt(score['dateTimeGMT']))
-      result[:date] = result[:time_raw].strftime("%B %-d, %Y")
-      result[:time_dirty] = result[:time_raw].strftime('%l %p')
-      result[:schedule].gsub!(/at\s(.*?\stime)/, "at#{result[:time_dirty]} Sydney time (\\1)")
+      result[:time] = gmttime(score['dateTimeGMT'])
+      result[:date] = gmtdate(score['dateTimeGMT'])
+      result[:schedule].gsub!(/at\s(.*?\stime)/, "at #{result[:time]} Sydney time (\\1)")
       result[:schedule].gsub!(/\(\d\d:\d\d GMT\)/, "")
-      result[:final] = "#{result[:schedule]}against *#{result[:team]}* on #{result[:date]}." << ' '
+      result[:final] = "#{result[:schedule]}against #{result[:team]} on #{result[:date]}." << ' '
       result[:final]
     elsif score['innings-requirement'].include?('scheduled') && score['team-1'].include?(team)
       result[:schedule] = score['innings-requirement']
       result[:team] = score['team-2']
-      result[:time_raw] = Time.parse(gmt(score['dateTimeGMT']))
-      result[:date] = result[:date_raw].strftime("%B %-d, %Y")
-      result[:time_dirty] = result[:time_raw].strftime('%l %p')
-      result[:schedule].gsub!(/at\s(.*?\stime)/, "at#{result[:time_dirty]} Sydney time (\\1)")
+      result[:time] = gmttime(score['dateTimeGMT'])
+      result[:date] = gmtdate(score['dateTimeGMT'])
+      result[:schedule].gsub!(/at\s(.*?\stime)/, "at #{result[:time]} Sydney time (\\1)")
       result[:schedule].gsub!(/\(\d\d:\d\d GMT\)/, "")
-      result[:final] = "#{result[:schedule]}against *#{result[:team]}* on #{result[:date]}." << ' '
+      result[:final] = "#{result[:schedule]}against #{result[:team]} on #{result[:date]}." << ' '
       result[:final]
     elsif score['matchStarted'] == true && !score['innings-requirement'].include?('toss') && !score['innings-requirement'].include?('won')
       score_dirty = score['score']
@@ -60,8 +80,8 @@ module Cric
       result[:final] = "#{score_clean} - #{rr} RPO. \n \n#{required}"
       result[:final]
     elsif score['matchStarted'] == true && !score['innings-requirement'].include?('toss') && score['innings-requirement'].include?('won')
-      result[:date_raw] = Time.parse(gmt(score['dateTimeGMT']))
-      result[:date] = result[:date_raw].strftime('%A %e %B at %l %p Sydney time.')
+      result[:date_raw] = Time.parse(gmttime(score['dateTimeGMT']))
+      result[:date] = result[:date_raw].strftime('%A %e %B.')
       result[:final] = "#{score['innings-requirement']} on #{result[:date]}"
       result[:final]
     elsif score['matchStarted'] == true
