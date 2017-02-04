3050089f3b33f2ea8c42b8e986fd1e3bd20ee077
Cameron Regan <cameronthomasregan@gmail.com>
2017-02-04 21:34:45 +1100

    Started creation on aflscore function

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ require './modules.rb'
 
 bot = Discordrb::Commands::CommandBot.new token: '***REMOVED***', client_id: ***REMOVED***, prefix: '!'
 
-bot.command :score do |event, *team|
+bot.command :cricket do |event, *team|
   newteam = team.join(" ")
   score = Cric.cricket_score(newteam)
   event.respond "#{score}"
@@ -26,12 +26,10 @@ bot.command(:commands, max_args: 0) do |event|
   event.user.pm "To fetch the score for a match, do '!score <team name>', e.g. '!team Australia'."
 end
 
-
-
-bot.command(:clear, min_args: 1)
-
-# bot.command(:invite, chain_usable: false) do |event|
-#  event.bot.invite_url
-# end
+bot.command :afl do |event, *team|
+  newteam = team.join(" ")
+  score = Afl.afl_score(newteam)
+  event.respond "#{score}"
+end
 
 bot.run
--- modules.rb
+++ modules.rb
@@ -8,47 +8,47 @@ require 'net/http'
 
 # Module for checking cricket scores/schedules using the CricAPI
 module Cric
-#  def self.gmt(time = nil) # don't use local_to_utc - doesn't make sense
-#    tz = TZInfo::Timezone.get('Australia/Sydney')
-#    time += ' UTC'
-#    time = Time.parse(time)
-#    time = tz.local_to_utc(time)
-#    tz.strftime('%Y-%m-%d %H:%M:%S %Z', time)
-#  end
+  #  def self.gmt(time = nil) # don't use local_to_utc - doesn't make sense
+  #    tz = TZInfo::Timezone.get('Australia/Sydney')
+  #    time += ' UTC'
+  #    time = Time.parse(time)
+  #    time = tz.local_to_utc(time)
+  #    tz.strftime('%Y-%m-%d %H:%M:%S %Z', time)
+  #  end
 
-#def self.gmt(t = nil) # convert GMT time to Sydney time (AEDT/AEST)
-#  t2 = tz.utc_to_local(t1)
-#  #t2 = t1 + t1.utc_offset # parse gmt_offset to local Sydney time
-#  t3 = t2.strftime("%F %T") # remove redundant gmt_offset
-#  t4 = t3.strftime("%I:%M %p") # convert to "hour:second PM/AM"
-#end
+  #def self.gmt(t = nil) # convert GMT time to Sydney time (AEDT/AEST)
+  #  t2 = tz.utc_to_local(t1)
+  #  #t2 = t1 + t1.utc_offset # parse gmt_offset to local Sydney time
+  #  t3 = t2.strftime("%F %T") # remove redundant gmt_offset
+  #  t4 = t3.strftime("%I:%M %p") # convert to "hour:second PM/AM"
+  #end
 
-def self.gmttime(tstr) # convert GMT time to timezone defined by tz variable
-  tz = TZInfo::Timezone.get('Australia/Sydney') # timezone to convert to
-  time = Time.parse(tstr) # parse time 
-  tz.utc_to_local(time).strftime("%I:%M %p") # convert to "hour:second AM/PM" and timezone
-end
+  def self.gmttime(tstr) # convert GMT time to timezone defined by tz variable
+    tz = TZInfo::Timezone.get('Australia/Sydney') # timezone to convert to
+    time = Time.parse(tstr) # parse time
+    tz.utc_to_local(time).strftime("%I:%M %p") # convert to "hour:second AM/PM" and timezone
+  end
 
-def self.gmtdate(tstr)
-  tz = TZInfo::Timezone.get('Australia/Sydney') # timezone to convert to
-  time = Time.parse(tstr) # parse time 
-  tz.utc_to_local(time).strftime("%B %d, %Y") # convert to "hour:second AM/PM" and timezone
-end
+  def self.gmtdate(tstr)
+    tz = TZInfo::Timezone.get('Australia/Sydney') # timezone to convert to
+    time = Time.parse(tstr) # parse time
+    tz.utc_to_local(time).strftime("%B %d, %Y") # convert to "hour:second AM/PM" and timezone
+  end
 
-  def self.fetch_matches
+  def self.fetch_matchescric
     uri = URI('http://cricapi.com/api/matches/?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2') # fetch json for match list
     JSON.parse(Net::HTTP.get(uri))['matches'] # parse match list as hash
   end
 
-  def self.fetch_score(id)
+  def self.fetch_scorecric(id)
     uri = URI("http://cricapi.com/api/cricketScore?apikey=1iHvVPXRR5RHEArkjQlIntupKkb2&unique_id=#{id}") # fetch json for matchs core
     JSON.parse(Net::HTTP.get(uri)) # parse score list as hash
   end
 
   def self.cricket_score(team) # Dont set team=nil, its not optional
-    id = fetch_matches.find { |h| h.key(team) }['unique_id']
+    id = fetch_matchescric.find { |h| h.key(team) }['unique_id']
 
-    score = fetch_score(id)
+    score = fetch_scorecric(id)
     result = {}
 
     if score.key?('score') && ['score'].include?('Match')
@@ -115,4 +115,61 @@ end
 
 # Module for checking live AFL scores and past games
 module Afl
+
+
+  def self.afl_score(team)
+
+    tz = TZInfo::Timezone.get('Australia/Sydney')
+
+    teamnames = {'Carlton' => 'CARL', 'Richmond' => 'RICH', 'Collingwood' => 'COLL', 'Bulldogs' => 'WB', 'St Kilda' => 'STK', 'Melbourne' => 'MEL', 'Sydney' => 'SYD', 'Port Adelaide' => 'PORT', 'Gold Coast Suns' => 'GC', 'Brisbane Lions' => 'BRIS', 'Essendon' => 'ESS', 'Hawthorn' => 'HAW', 'North Melbourne' => 'NMFC', 'West Coast' => 'WCE', 'Adelaide' => 'ADEL', 'GWS' => 'GWS', 'Fremantle' => 'FRE', 'GEEL' => 'Geelong'}
+
+    if teamnames.include?(team)
+      shortteamdirty = teamnames.select{|k,v| k == team}
+      shortteamclean = shortteamdirty[team]
+    end
+
+
+    uri = URI('http://afl.lookoutdata.com/live/feeds/m//widget/1/default/') # fetch json for matchs core
+    matchesdirty = JSON.parse(Net::HTTP.get(uri)) # parse json as hash
+    matchesclean = matchesdirty['c']['matches']
+
+    matchentrydirty = matchesclean.find {|v| v = shortteamclean}
+    matchentryclean = matchentrydirty['quickScore']
+
+    result = {}
+
+    if matchentryclean['homeTeamName'] == shortteamclean
+      id = matchentryclean['matchID']
+    elsif matchentryclean['awayTeamName'] == shortteamclean
+      id = matchentryclean['matchID']
+    end
+
+  
+    result[:homeTeamDirty] = matchentryclean['homeTeamName']
+    result[:homeTeam] =  teamnames.select {|_, v| v == result[:homeTeamDirty]}.keys[0]
+   
+    result[:awayTeamDirty] = matchentryclean['awayTeamName']
+    result[:awayTeam] = teamnames.select {|_, v| v == result[:awayTeamDirty]}.keys[0]
+
+    result[:homeScore] = matchentryclean['homeScore']
+    result[:awayScore] = matchentryclean['awayScore']
+    result[:venue] = matchentryclean['venueName']
+
+    start_time_dirty = Time.parse(matchentryclean['utcStartTime'])
+    start_time_dirty_converted = tz.utc_to_local(start_time_dirty)
+    start_time_clean = start_time_dirty_converted.strftime('%I:%M %p')
+
+    result[:starttime] = start_time_clean
+
+    start_date_dirty = Time.parse(matchentryclean['utcStartTime'])
+    start_date_dirty_converted = tz.utc_to_local(start_date_dirty)
+    start_date_clean = start_date_dirty_converted.strftime('%b %d, %Y')
+
+    result[:startdate] = start_date_clean
+
+    result[:final] = "#{result[:homeTeam]}: #{result[:homeScore]} - #{result[:awayTeam]}: #{result[:awayScore]}"
+
+
+  end
+
 end
