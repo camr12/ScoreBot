6df1a604d53966af6b69c0c4925a8d0e0b1cbf93
Cameron Regan <cameronthomasregan@gmail.com>
2017-02-28 21:25:50 +1100

    put rate limit in, integrated new afl modules

--- main.rb
+++ main.rb
@@ -10,7 +10,9 @@ require './modules.rb'
 
 bot = Discordrb::Commands::CommandBot.new token: '***REMOVED***', client_id: ***REMOVED***, prefix: '!'
 
-bot.command :cricket do |event, *team|
+bot.bucket :afl, limit: 1, time_span:1800
+
+bot.command(:cricket, bucket: :afl) do |event, *team|
   newteam = team.join(" ")
   score = Cric.cricket_score(newteam)
   event.respond "#{score}"
@@ -26,9 +28,15 @@ bot.command(:commands, max_args: 0) do |event|
   event.user.pm "To fetch the score for a match, do '!score <team name>', e.g. '!team Australia'."
 end
 
-bot.command :afl do |event, *team|
+#bot.command :afl do |event, *team|
+#  newteam = team.join(" ")
+#  score = Afl.afl_score(newteam)
+#  event.respond "#{score}"
+#end
+
+bot.command(:afl, bucket: :afl) do |event, *team|
   newteam = team.join(" ")
-  score = Afl.afl_score(newteam)
+  score = Afl.get_id(newteam)
   event.respond "#{score}"
 end
 
