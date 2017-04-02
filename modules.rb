b03cddc695092798fb7f2a826b12cf3908f32f59
Cameron Regan <cameronthomasregan@gmail.com>
2017-04-02 13:56:44 +1000

    fixed regex

--- modules.rb
+++ modules.rb
@@ -177,7 +177,7 @@ module Afl
 def self.get_id(team)
   team = team.split.map(&:capitalize).join(' ')
   viewgames = open("http://dtlive.com.au/afl/viewgames.php").read
-  viewgames[/GameID=(\d+)">[^>]+> #{team}/]
+  viewgames[/GameID=(\d+)">[^>]+>([^>]+>)? #{team}/]
 
   gameid = $1
 
