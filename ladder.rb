695d26f496e3ba4667b21f101b6a59e026fe887a
Cameron Regan <cameronthomasregan@gmail.com>
2017-05-13 21:06:50 +1000

    changed filename

--- ladder.rb
+++ ladder.rb
@@ -2,13 +2,13 @@ require 'gastly'
 require 'date'
 while true
   begin
-    screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '.tablesorter-green', timeout:1800000)
-    image = screenshot.capture
-    image.save('live.png')
-    currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
-    puts "Saved live ladder at #{currenttime}"
-  rescue Ghastly::FetchError => e
-    puts "Got an exception, restarting."
-    next
+      screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '.tablesorter-green', timeout:1800000)
+      image = screenshot.capture
+      image.save('ladder.png')
+      currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
+      puts "Saved live ladder at #{currenttime}"
+      rescue Ghastly::FetchError => e
+        puts "Got an exception, restarting."
+        next
+      end
   end
-end
