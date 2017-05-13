e1a21d2e8ec28dea908644c55bf5d5247219728d
Cameron Regan <cameronthomasregan@gmail.com>
2017-05-13 20:59:29 +1000

    fixed exception

--- live.rb
+++ live.rb
@@ -1,9 +1,14 @@
 require 'gastly'
+require 'date'
 while true
-  screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green')
-rescue
-  next
-  image = screenshot.capture
-  image.save('live.png')
-end
+  begin
+    screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green', timeout:1000)
+    image = screenshot.capture
+    image.save('live.png')
+    currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
+    puts "Saved live ladder at #{currenttime}"
+  rescue Ghastly::FetchError => e
+    puts "Got an exception, restarting."
+    next
+  end
 end
