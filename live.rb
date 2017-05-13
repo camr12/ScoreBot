3b339b54bfe541c4111f41dcfb6770296709c728
Cameron Regan <cameronthomasregan@gmail.com>
2017-05-13 11:18:04 +1000

    added exception

--- live.rb
+++ live.rb
@@ -1,7 +1,14 @@
 require 'gastly'
 while true
-screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green')
-image = screenshot.capture
-image.save('live.png')
+  screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green')
+  image = screenshot.capture
+  image.save('live.png')
 end
 
+rescue Gastly::FetchError
+  while true
+    screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green')
+    image = screenshot.capture
+    image.save('live.png')
+  end
+end
