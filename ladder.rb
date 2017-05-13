10ea287eb211305650ce68f78a3dbdb3c2a401df
Cameron Regan <cameronthomasregan@gmail.com>
2017-05-13 11:21:42 +1000

    fixed exception

--- ladder.rb
+++ ladder.rb
@@ -1,14 +1,9 @@
 require 'gastly'
 while true
   screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table', timeout: 1000)
+rescue
+  next
   image = screenshot.capture
   image.save('ladder.png')
 end
-
-rescue Gastly::FetchError
-  while true
-    screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table', timeout: 1000)
-    image = screenshot.capture
-    image.save('ladder.png')
-  end
 end
