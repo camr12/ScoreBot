9a92e729030e5adacfb40cadf7998542b85e3674
Cameron Regan <cameronthomasregan@gmail.com>
2017-06-03 18:56:13 +1000

    undid previous commit, broken

--- main.rb
+++ main.rb
@@ -63,35 +63,7 @@ bot.command(:afl) do |event, *team|
   score = Afl.process_feed(newteam)
   event.respond "#{score}"
 end
-bot.command(:AFL) do |event, *team|
-   zedteams = {"Adelaide" => ["adelaide", "crows", "ade", "adel"],
-             "Brisbane" => ["brisbane", "lions", "bl", "bris", "fitzroy"],
-             "Carlton" => ["carlton", "blues", "car", "carl"],
-             "Collingwood" => ["collingwood", "magpies", "pies", "col", "coll"],
-             "Essendon" => ["essendon", "bombers", "ess"],
-             "Fremantle" => ["fremantle", "dockers", "fre", "freo"],
-             "Geelong" => ["geelong", "cats", "gee", "geel"],
-             "Gold Coast" => ["gold coast", "suns", "gc", "gcfc"],
-             "GWS Giants" => ["greater western sydney", "giants", "gws"],
-             "Hawthorn" => ["hawthorn", "hawks", "haw"],
-             "Melbourne" => ["melbourne", "demons", "dees", "mel", "melb"],
-             "North Melbourne" => ["north melbourne", "kangaroos", "roos", "nmfc", "norf", "north"],
-             "Port Adelaide" => ["port adelaide", "power", "port", "pa", "pafc", "pear"],
-             "Richmond" => ["richmond", "tigers", "rich", "tiges", "ninthmond"],
-             "St Kilda" => ["st kilda", "saints", "stk", "street kilda"],
-             "Sydney" => ["sydney", "swans", "syd", "south melbourne", "smfc", "bloods"],
-             "West Coast" => ["west coast", "eagles", "wce", "wc", "weagles"],
-             "Western Bulldogs" => ["western bulldogs", "bulldogs", "dogs", "wb", "footscray"]} 
-  newteam = team.join(" ").downcase
-   unless zedteams.values.flatten.include?(newteam)
-        bot.send_temporary_message(event.channel.id, content = "#{event.author.mention}: \<:bt:246541254182174720> THAT WAS OUT OF BOUNDS! `#{newteam}` is not an accepted input!", timeout = 10)
-        sleep 10
-        event.message.delete
-        raise ArgumentError.new("THAT WAS OUT OF BOUNDS!")
-    end
-  score = Afl.process_feed(newteam)
-  event.respond "#{score}"
-end
+
 bot.command(:a, bucket: :afl) do |event|
   newteam = "Australia"
   score = Cric.cricket_score(newteam)
