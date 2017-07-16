d4db3c36eefcd4c8db287d97bf5aac88c12af53f
Cameron Regan <cameronthomasregan@gmail.com>
2017-07-16 16:45:16 +1000

    Added a single-time instance of the ladder-save block in ladder files

--- ladder.rb
+++ ladder.rb
@@ -1,14 +1,16 @@
 require 'gastly'
 require 'date'
+
+begin
+  screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table')
+  image = screenshot.capture
+  image.save('ladder.png')
+  currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
+  puts "Saved AFL ladder at #{currenttime}"
+rescue Ghastly::FetchError => e
+  puts "Got an exception, restarting."
+end
+
 while true
   begin
-      screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table', timeout:1800000)
-      image = screenshot.capture
-      image.save('ladder.png')
-      currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
-      puts "Saved AFL ladder at #{currenttime}"
-      rescue Ghastly::FetchError => e
-        puts "Got an exception, restarting."
-        next
-      end
-  end
+    screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table', timeout:1800000)
--- live.rb
+++ live.rb
@@ -1,14 +1,16 @@
 require 'gastly'
 require 'date'
+
+begin
+  screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green')
+  image = screenshot.capture
+  image.save('live.png')
+  currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
+  puts "Saved live ladder at #{currenttime}"
+rescue Ghastly::FetchError => e
+  puts "Got an exception, restarting."
+end
+
 while true
   begin
     screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green', timeout:300000)
-    image = screenshot.capture
-    image.save('live.png')
-    currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
-    puts "Saved live ladder at #{currenttime}"
-  rescue Ghastly::FetchError => e
-    puts "Got an exception, restarting."
-    next
-  end
-end
