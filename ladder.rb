652ee657451490f62a4eb2bc7ce38cc3a0b5777d
Cameron Regan <cameronthomasregan@gmail.com>
2017-07-16 18:02:06 +1000

    Bug fix, missing code

--- ladder.rb
+++ ladder.rb
@@ -14,3 +14,11 @@ end
 while true
   begin
     screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table', timeout:1800000)
+    image = screenshot.capture
+    image.save('ladder.png')
+    currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
+    puts "Saved AFL ladder at #{currenttime}"
+  rescue Ghastly::FetchError => e
+    puts "Got an exception, restarting."
+  end
+end
--- live.rb
+++ live.rb
@@ -14,3 +14,11 @@ end
 while true
   begin
     screenshot = Gastly.screenshot('http://liveladders.com/afl/', selector: '.tablesorter-green', timeout:300000)
+    image = screenshot.capture
+    image.save('live.png')
+    currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
+    puts "Saved live ladder at #{currenttime}"
+  rescue Ghastly::FetchError => e
+    puts "Got an exception, restarting."
+  end
+end
