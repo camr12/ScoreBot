81b708df260f738a3c9863a29cc75d7249be2b90
Cameron Regan <cameronthomasregan@gmail.com>
2018-03-24 18:02:24 +1100

    Maybe fixed ladder.

--- ladder.rb
+++ ladder.rb
@@ -2,7 +2,7 @@ require 'gastly'
 require 'date'
 
 begin
-  screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table')
+  screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#c-live-ladder')
   image = screenshot.capture
   image.save('ladder.png')
   currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
@@ -13,7 +13,7 @@ end
 
 while true
   begin
-    screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#ladder-table', timeout:1800000)
+    screenshot = Gastly.screenshot('http://afl.com.au/ladder', selector: '#c-live-ladder', timeout:1800000)
     image = screenshot.capture
     image.save('ladder.png')
     currenttime = Time.now.strftime("%d/%m/%Y %H:%M:%-S")
